12e5c27e6de7c6caa4a4f3f13b5904feb35a83f9
regin0 <regino9006@gmail.com>
2020-09-23 22:40:06 -0300

    Aula do yoshi de como colocar e mudar imagem

--- Yoshi/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Yoshi/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.myapplication
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var cliques = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun contar(componente: View){
+        cliques++
+
+        tv_contador.text = "Cliques: $cliques"
+
+
+        if (cliques == 5){
+            id_foto.setImageDrawable(getDrawable(R.mipmap.flash_reverso))
+        }
     }
 }
